b1e2cf996baa32bcf119f5df213e7266f5deef22
Joey <joseph.lorusso1@marist.edu>
2020-05-16 18:47:14 -0400

    Working on onClick()

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function clickItem(itemNum){
+    window.alert(itemNum);
+}
+
+function updateItems(){
+    var image1 = document.getElementById(img1);
+    var image2 = document.getElementById(img2);
+    
+    image1.src = "https://images-na.ssl-images-amazon.com/images/I/71APeLt-jaL._AC_SL1088_.jpg";
+    
+    image2.src = "https://cdn.cancercenter.com/-/media/ctca/images/others/blogs/2019/08-august/04-blog-grapefruit-juice-l.jpg?h=630&la=en&w=1200&hash=38967FA3C9D017D66A4C360D4185099B";
+}
