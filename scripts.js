bd8f06251b54a6f9e987f0dd4fa0615a59f7e9d9
Emily Gitlin <ergitlin@Emilys-MacBook-Pro-3.local>
2020-05-16 19:34:11 -0400

    Added comments and function stubs

--- scripts.js
+++ scripts.js
@@ -1,13 +1,32 @@
 function clickItem(itemNum){
-    window.alert(itemNum);
+    window.alert(itemNum); //adds popup
     updateItems();
 }
 
 function updateItems(){
+    //declare image1 and image2 which are
     var image1 = document.getElementById('img1');
     var image2 = document.getElementById('img2');
-    
+
     image1.src = "https://images-na.ssl-images-amazon.com/images/I/71APeLt-jaL._AC_SL1088_.jpg";
-    
+
     image2.src = "https://cdn.cancercenter.com/-/media/ctca/images/others/blogs/2019/08-august/04-blog-grapefruit-juice-l.jpg?h=630&la=en&w=1200&hash=38967FA3C9D017D66A4C360D4185099B";
 }
+//Things we need to do:
+
+//update the count for the word
+function updateCount(){
+
+}
+
+//get 2 new words
+function wordGrab(){
+
+}
+
+//finds image for those words
+function image69(){
+
+}
+//sets image1 and image2 to be those images, and the text to be that text
+//this is done in update items I think
